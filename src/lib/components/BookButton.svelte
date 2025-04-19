bf5bcfe16517fe0e2839a55dde62bc2eb9bf9ced
Natouche68 <ngrossheny@gmail.com>
2025-04-19 19:41:54 +0200

    Update the book stack to make it smaller

---
+++ src/lib/components/BookButton.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	interface Props {
+		title: string;
+		author: string;
+		coverImg: string;
+	}
+
+	let { title, author, coverImg }: Props = $props();
+</script>
+
+<div class="inline-block w-48 my-4 mx-2">
+	<div class="w-48 h-64 mb-2">
+		<img
+			src={coverImg}
+			alt="{title} de {author}"
+			class="w-full h-full object-cover"
+		/>
+	</div>
+	<div class="text-xl font-bold font-serif">{title}</div>
+	<div class="italic">{author}</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,35 @@
 <script lang="ts">
 	import BookCard from "$lib/components/BookCard.svelte";
+	import BookButton from "$lib/components/BookButton.svelte";
+
+	const books = $state([
+		{
+			title: "36 façons de tuer",
+			author: "Stefen Ahnhem",
+			coverImg:
+				"https://static.fnac-static.com/multimedia/PE/Images/FR/NR/b7/a4/f3/15967415/1507-1/tsp20240105073935/36-Facons-de-tuer.jpg",
+			buyingDate: 1745000233132,
+			pageNumber: 630,
+			tags: ["policier", "suédois"],
+		},
+		{
+			title: "Poulets grillés",
+			author: "Sophie Hénaff",
+			coverImg:
+				"http://www.mediathequesroannaisagglomeration.fr/sites/default/files/poulets_grilles_-_sophie_henaff.jpg",
+			buyingDate: 1744136180376,
+			pageNumber: 330,
+			tags: ["policier"],
+		},
+		{
+			title: "Rester groupés",
+			author: "Sophie Hénaff",
+			coverImg: "https://m.media-amazon.com/images/I/81aU0zEBFJL._SL1500_.jpg",
+			buyingDate: 1744136180376,
+			pageNumber: 310,
+			tags: ["policier"],
+		},
+	]);
 </script>
 
 <div class="min-h-dvh flex flex-col bg-green-50">
@@ -19,7 +49,19 @@
 		pagesRead={320}
 	/>
 
-	<div class="h-full flex flex-col">
+	<div class="mx-4 mt-4 text-3xl font-bold font-serif">Pile de livres</div>
+
+	<div class="overflow-x-auto whitespace-nowrap px-2">
+		{#each books as book}
+			<BookButton
+				title={book.title}
+				author={book.author}
+				coverImg={book.coverImg}
+			/>
+		{/each}
+	</div>
+
+	<!-- <div class="h-full flex flex-col">
 		<BookCard
 			title="36 façons de tuer"
 			author="Stefen Ahnhem"
@@ -44,5 +86,5 @@
 			pageNumber={310}
 			tags={["policier"]}
 		/>
-	</div>
+	</div> -->
 </div>
